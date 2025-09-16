b121478b3308a12032f0082b20f95886da7fece3
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-09-16 14:41:59 +0530

    Implemented how would i return the answer in form of an array

--- Two Pointers/TwoSum.java
+++ Two Pointers/TwoSum.java
@@ -4,7 +4,13 @@ public class TwoSum {
     public static void main(String[] args){
         int arr[]={2,7,11,15};
         int target =9;
-        findS(arr,target);
+        //findS(arr,target);
+        int result[]= findSA(arr, target);
+        if(result.length==0){
+            System.err.println("No 2 array elements adds upto the target sum");
+        }else{
+            System.out.println((result[0]+1)+" "+(result[1]+1));
+        }
     }
     public static void findS(int arr[], int target){
         int start=0;
@@ -21,5 +27,21 @@ public class TwoSum {
             }
         } 
     }
+    public static int[] findSA(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int sum=arr[start]+arr[end];
+            if(sum==target){
+                return new int[]{start,end};
+            }else if(sum>target){
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return new int []{};
+    }
+
 }
          
