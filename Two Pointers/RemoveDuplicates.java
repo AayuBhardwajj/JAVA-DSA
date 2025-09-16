7b0221735ec18bfb5feb9de83b94255ec3de9c64
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-09-16 15:15:32 +0530

    Remove Duplicate Elements in O(n) complexity

---
+++ Two Pointers/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+//package JAVA-DSA.Two Pointers;
+
+public class RemoveDuplicates {
+    public static void main(String[] args){
+        int arr[]={1,1,2};
+        noDup(arr);
+    }
+    public static void noDup(int arr[]){
+        int j=0;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]!=arr[i+1]){
+                arr[j++]=arr[i];
+            }
+        }
+            arr[j++]=arr[arr.length-1];
+            for(int i=0;i<j;i++){
+                System.err.print(arr[i]+" ");
+            }
+        }
+    }
+
+
