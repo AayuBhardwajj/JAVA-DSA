ca4f2c8e7588fb545e9213f11561c329611fad22
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-10-24 10:18:42 +0530

    Remove Duplicated from sorted array using two pointers

--- Arrays/RemDupFrSrAr.java
+++ Arrays/RemDupFrSrAr.java
@@ -3,6 +3,7 @@ public class RemDupFrSrAr {
     public static void main(String[] args){
         int arr[]={1,1,2,2,3,4,4,4,5};
         removeTheDuplicateElements(arr);
+        twoPoint(arr);
 
     }
     public static void removeTheDuplicateElements(int arr[]){
@@ -14,4 +15,21 @@ public class RemDupFrSrAr {
         System.out.print(i+" ");
       }
 }
+
+    public static void twoPoint(int arr[]){
+        int i=0;
+        int count=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j]!=arr[i]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        System.out.println();
+        for(int k=0;k<=i;k++){
+            System.out.println(arr[k]+" ");
+        }
+        System.out.println();
+        System.out.print(i+1);
+    }
 }
