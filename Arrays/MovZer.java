9527920d4848d6933c77f07c88f8d19bb76ee33c
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-10-27 13:54:08 +0530

    Move zeroes to the back, optimized approach

--- Arrays/MovZer.java
+++ Arrays/MovZer.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 public class MovZer {
     public static void main(String[] args) {
         int arr[]={1,0,2,0,3,0,4,0,5,0,6,0};
-        moveZeroesToEnd(arr);
+        //moveZeroesToEnd(arr);
+        pushZeroesToBack(arr);
         printAr(arr);
     }
 
@@ -28,4 +29,27 @@ public class MovZer {
         }
 
     }
-}
+
+    public static void swap(int i, int j, int arr[]){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    public static void pushZeroesToBack(int arr[]){
+        int x=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                x=i;
+                break;
+            }
+        }
+            for(int i=x+1;i<arr.length;i++){
+                if(arr[i]!=0){
+                    swap(x, i, arr);
+                    x++;
+                }
+            }
+        }
+    }
+
