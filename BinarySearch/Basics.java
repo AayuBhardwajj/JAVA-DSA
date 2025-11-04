e5f5cafe304e5c4eeb25ddb07614a7c13ecc4b5c
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-11-04 21:55:34 +0530

    Updated the basic code of binary search

--- BinarySearch/Basics.java
+++ BinarySearch/Basics.java
@@ -4,20 +4,22 @@ public class Basics {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8};
         int target=4;
-        findTheIndexOfTarget(arr, target);
+        System.out.println(findTheIndexOfTarget(arr, target));
     }
-    public static void findTheIndexOfTarget(int arr[], int target){
+    public static int findTheIndexOfTarget(int arr[], int target){
         int start=0;
         int end=arr.length-1;
         while(start<=end){
-            int mid=(start+end)/2;
+            int mid=start+(end-start)/2;
             if(arr[mid]==target){
-                System.out.println(target+"found at index: "+mid);;
-            }else if(arr[mid]>targer){
+                System.out.println(target+" found at index: "+mid);;
+               return mid;
+            }else if(arr[mid]>target){
                 end=mid-1;
             }else{
                 start=mid+1;
             }
         }
+        return -1;
     }
 }
