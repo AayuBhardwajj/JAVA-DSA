3786533b2af27100403c4935ba730db55b2f414c
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-01-04 13:17:35 +0530

    Find element in rotated sorted array using binary search

---
+++ BinarySearch/RotSortBs.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+public class RotSortBs {
+    public static void main(String[] args){
+        int arr[]={7,8,9,1,2,3,4};
+        int target=1;
+        System.out.println(findTargetUsingBinarySearch(arr, target));
+    }
+    public static int findTargetUsingBinarySearch(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[start]<=arr[mid]){
+                if(arr[start]<= target && target<=arr[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                if(arr[mid]<= target && target<=arr[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
