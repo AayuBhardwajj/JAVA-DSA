e4492a35bef9c0f97894661153b4811a6dbc630b
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-01-06 15:47:57 +0530

    Understood the concept of finding number of times an array was rotated using brute force approach

---
+++ BinarySearch/FindMin.java
@@ -0,0 +1,18 @@
+package BinarySearch;
+public class FindMin{
+    public static void main(String[] args){
+        int arr[]={3,4,5,1,2};
+        findRot(arr);
+    }
+    public static void findRot(int arr[]){
+        int min=Integer.MAX_VALUE;
+        int minInd=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+                minInd=i;
+            }
+        }
+        System.out.println("Array was rotated "+minInd+" times");
+    } 
+}
