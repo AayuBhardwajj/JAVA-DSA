a053af0fbaf646c6164dba5474ad87c168c6152b
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-03-10 09:42:05 +0530

    Palindrome Linked List

--- Arrays/rty.java
+++ Arrays/rty.java
@@ -2,5 +2,8 @@ public class rty{
     public static void main(String[] args) {
         System.out.println("a"+7);
         System.out.println(7+'a');
+        int a=12;
+        System.out.println(a%10);
+        System.out.println(a/10);
     }
 }
--- Linked List/FindMid.java
+++ Linked List/FindMid.java
@@ -1,17 +1,37 @@
-class Node {
-    int data;
-    Node next;
-    
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class FindMid {
 
-    public static Node findMiddle(Node head) {
-        
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+
+    public void addNode(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+
+    public Node findMiddle() {
+
         if (head == null)
             return null;
 
@@ -19,21 +39,28 @@ public class FindMid {
         Node fast = head;
 
         while (fast != null && fast.next != null) {
-            slow = slow.next;          // move 1 step
-            fast = fast.next.next;     // move 2 steps
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
         return slow;
     }
 
     public static void main(String[] args) {
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = new Node(5);
 
-        Node mid = findMiddle(head);
-        System.out.println("Middle Node: " + mid.data);
+        FindMid list = new FindMid();
+
+        list.addNode(1);
+        list.addNode(2);
+        list.addNode(3);
+        list.addNode(4);
+        list.addNode(5);
+
+        Node mid = list.findMiddle();
+
+        if (mid != null)
+            System.out.println("Middle Node: " + mid.data);
+        else
+            System.out.println("List is empty");
     }
 }
