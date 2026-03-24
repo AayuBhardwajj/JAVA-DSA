72b08bb31183b478ab45f7e7e284042b97a2588d
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-03-24 12:24:48 +0530

    Updated code

--- Recursion/IfSorted.java
+++ Recursion/IfSorted.java
@@ -2,15 +2,15 @@ package Recursion;
 
 public class IfSorted {
     public static void main(String[] args){
-        int arr[]={1,2,3,4,5,6,7};
+        int arr[]={1,2,3,47,5,6,7};
         System.out.println(isSorted(arr, 6));
     }
     public static boolean isSorted(int arr[],int n){
         if(n==0 || n==1){
             return true;
         }
-        if(arr[n-1]>arr[n-2]){
-            return true;
+        if(arr[n-1]<arr[n-2]){
+            return false;
         }
         return isSorted(arr, n-1);
     }
