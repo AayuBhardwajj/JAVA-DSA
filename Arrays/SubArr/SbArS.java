385a454040ac5e86f9b97d391a13665e7cc392d7
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-04-07 11:51:07 +0530

    Revised how to find number of subsets whose sum/product is greater/lesser than any given value k

---
+++ Arrays/SubArr/SbArS.java
@@ -0,0 +1,26 @@
+public class SbArS {
+    public static void main(String[] args) {
+        int arr[]={1,2,3};
+        findProductOfSubArray(arr);
+        findSumOfSubArray(arr);
+    }
+    public static void findSumOfSubArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int s=0;
+            for(int j=i;j<arr.length;j++){
+                s+=arr[j];
+                System.out.println("Sum: "+s);
+            }
+        }
+    }
+
+    public static void findProductOfSubArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int p=1;
+            for(int j=i;j<arr.length;j++){
+                p*=arr[j];
+                System.out.println("Product :"+p);
+            }
+        }
+    }
+}
---
+++ Arrays/SubArr/SbArS1.java
@@ -0,0 +1,20 @@
+package SubArr;
+
+public class SbArS1 {
+    public static void main(String[] args){
+        int arr[]={1,2,3};
+        int value=10;
+         int count=0;
+        for(int i=0;i<arr.length;i++){
+           
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                if(sum<value){
+                    count++;
+                }
+            }
+        }
+System.out.println(count);
+    }
+}
