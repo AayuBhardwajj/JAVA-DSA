de99814450c1567c50995cee5ab7583d2959f8f7
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-04-08 10:10:09 +0530

    Understood the sliding window approach, using hashmap

--- Arrays/SubArr/FruitsInBasket.java
+++ Arrays/SubArr/FruitsInBasket.java
@@ -1,11 +1,16 @@
 package SubArr;
 import java.util.HashSet;
+import java.util.HashMap;
 public class FruitsInBasket {
     public static void main(String[] args){
         int arr[]={1,1,2,1,3,3,3,3};
         int k=2;
+        brute(arr, k);
+        op(arr);
+    }
+    public static void brute(int arr[], int k){
         int minL=Integer.MIN_VALUE;
-       
+    
         for(int i=0;i<arr.length;i++){
              HashSet <Integer> hs= new HashSet<>();
             for(int j=i;j<arr.length;j++){
@@ -19,6 +24,26 @@ public class FruitsInBasket {
             }
         }
         System.out.println(minL);
+    }
+
+    public static void op(int arr[]){
+        HashMap<Integer, Integer> hm= new HashMap<>();
+        int left=0;
+        int maxL=Integer.MIN_VALUE;
+        for(int right=0;right<arr.length;right++){
+            hm.put(arr[right], hm.getOrDefault(arr[right], 0)+1);
+
+            while(hm.size()>2){
+                hm.put(arr[left], hm.get(arr[left])-1);
+                
+                if(hm.get(arr[left])==0){
+                    hm.remove(arr[left]);
+                }
+                left++;
+            }
+            maxL=Math.max(maxL, right-left+1);
+        }
+        System.out.println(maxL);
     }
     
 }
