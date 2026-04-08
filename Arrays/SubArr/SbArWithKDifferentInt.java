0871579e70a836358d0418a463bf62cc8b5e3928
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-04-08 11:14:23 +0530

    Sub Array with k different integers, using sliding window

--- Arrays/SubArr/SbArWithKDifferentInt.java
+++ Arrays/SubArr/SbArWithKDifferentInt.java
@@ -1,10 +1,13 @@
 package SubArr;
 import java.util.HashSet;
+import java.util.HashMap;
 public class SbArWithKDifferentInt {
     public static void main(String[] args){
         int arr[]={1,2,1,3,4};
         int k=3;
         findGoodSubArrays(arr, k);
+        int result=optimized(arr, k) - optimized(arr, k-1);
+        System.out.println(result);
     }
     public static void findGoodSubArrays(int arr[],int k){
         int max=0;
@@ -23,4 +26,24 @@ public class SbArWithKDifferentInt {
     }
      System.out.println(max);
 }
+
+    public static int optimized(int arr[], int k){
+        int count=0;
+        int j=0;
+        HashMap<Integer, Integer> hm= new HashMap<>(); 
+        for(int i=0;i<arr.length;i++){
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
+
+            while(hm.size()>k){
+                hm.put(arr[j], hm.get(arr[j])-1);
+                if(hm.get(arr[j])==0){
+                    hm.remove(arr[j]);
+                }
+                j++;
+            }
+           
+                count+=(i-j+1);
+        }
+        return count;
+    }
 }
