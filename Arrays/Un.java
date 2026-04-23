1e29c45b415db3131f126c34af3d0e7a62942656
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-04-23 23:44:54 +0530

    Implemented Union of arrays, using 2 pointers approach

--- Arrays/Un.java
+++ Arrays/Un.java
@@ -6,7 +6,8 @@ public class Un {
         int arr2[]={4,4,5,5,6,7};
         int m= arr1.length;
         int n= arr2.length;
-        findUnion(arr1, arr2, m, n);
+        //findUnion(arr1, arr2, m, n);
+        findUnionOptimized(arr1, arr2, m, n);
 
     }
     public static void findUnion(int arr1[], int arr2[], int m, int n){
@@ -19,4 +20,43 @@ public class Un {
         }
         System.out.println(hm);
     }
+
+    public static void findUnionOptimized(int arr1[], int arr2[], int m, int n){
+        ArrayList<Integer> al= new ArrayList<>();
+        int i=0;
+        int j=0;
+
+        while(i<m && j<n){
+            if(arr1[i]<arr2[j]){
+                if(al.size()==0 || al.get(al.size()-1)!=arr1[i]){
+                    al.add(arr1[i]);
+                }
+                i++;
+            } else if(arr2[j]<arr1[i]){
+                if(al.size()==0 || al.get(al.size()-1)!=arr2[j]){
+                    al.add(arr2[j]);
+                }
+                j++;
+            }else{
+                if(al.size()==0 || al.get(al.size()-1)!=arr1[i]){
+                    al.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+            while(i<m){
+                if(al.size()==0 ||al.get(al.size()-1)!=arr1[i]){
+                    al.add(arr1[i]);
+                }
+                i++;
+            }
+            while(j<n){
+                if(al.size()==0 ||al.get(al.size()-1)!=arr1[j]){
+                    al.add(arr2[j]);
+                }
+                 j++;
+            }
+            System.out.println(al);
+        }
 }
