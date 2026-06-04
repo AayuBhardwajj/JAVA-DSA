7c5425dfffe2f61806bf84d362281e9aac9528cc
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-06-04 14:56:37 +0530

    Brute force approach of Find Duplicate number

---
+++ Placement/DuplicateNum.java
@@ -0,0 +1,23 @@
+package Placement;
+
+public class DuplicateNum {
+    public static void main(String[] args){
+        int arr[]={1,3,4,4,2};
+        findDup(arr);
+    }
+    public static void findDup(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[j]==arr[i]){
+                    count++;
+                }
+            }
+        
+        if(count>1){
+            System.out.println("Duplicate element is: "+arr[i]);
+            return;
+        }
+    }
+}
+}
