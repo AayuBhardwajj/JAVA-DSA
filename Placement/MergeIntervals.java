5e7ad281ca33d713ed39c40d0eb669b2724031af
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-06-04 13:55:21 +0530

    Optimal approach of merge intervals

--- Placement/MergeIntervals.java
+++ Placement/MergeIntervals.java
@@ -7,7 +7,9 @@ public class MergeIntervals {
     public static void main(String[] args) {
         int arr[][]={{1,3},{2,4},{3,5},{6,7}};
         findMergeInterval(arr);
+        optimizedMergeInterval(arr);
     }
+
     public static void findMergeInterval(int arr[][]){
         Arrays.sort(arr, (a, b)->a[0] - b[0]);
         ArrayList<int []> al= new ArrayList<>();    
@@ -32,5 +34,25 @@ public class MergeIntervals {
         for(int[] x:al){
             System.out.println(Arrays.toString(x));
         }
+    }
+
+    public static void optimizedMergeInterval(int arr[][]){
+        Arrays.sort(arr, (a,b)-> a[0] - b[0]);
+        ArrayList <int[]>al= new ArrayList<>();
+        for(int x[]: arr){
+            if(al.isEmpty()){
+                al.add(x);
+            }
+            else if(x[0]>al.get(al.size()-1)[1]){
+                al.add(x);
+            }
+            else{
+                al.get(al.size()-1)[1]=
+                Math.max(x[1], al.get(al.size()-1)[1]);
+            }
+        }
+       for(int p[]:al ){
+        System.out.println(Arrays.toString(p));
+       }
     }
 }
