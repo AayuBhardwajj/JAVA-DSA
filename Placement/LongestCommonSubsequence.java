305b44b30f3b26c8dec9f216f97c49aa3dfa01e9
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-06-11 12:07:02 +0530

    Longest Common Subsequence , Optimal approach using HashSet

--- Placement/LongestCommonSubsequence.java
+++ Placement/LongestCommonSubsequence.java
@@ -1,11 +1,14 @@
 package Placement;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class LongestCommonSubsequence {
     public static void main(String[] args) {
         int arr[]={100,4,200,1,3,2};
-        System.out.println(longestCommon(arr));
+        // System.out.println(longestCommon(arr));
+        System.out.println(longOptimized(arr));
     }
     public static int longestCommon(int arr[]){ // brute force
         if(arr.length==0){
@@ -28,4 +31,26 @@ public class LongestCommonSubsequence {
        }
         return longest;
     }
+
+    public static int longOptimized(int arr[]){. // optimized approach
+        HashSet<Integer> hs = new HashSet<>();
+        int longest=1;
+        for(int i: arr){
+            hs.add(i);
+        }
+
+        for(int i: hs){
+            if(!hs.contains(i-1)){
+                int curr=i;
+                int count=1;
+            
+            while(hs.contains(curr+1)){
+                count++;
+                curr++;
+            }
+            longest=Math.max(count, longest);
+        }
+    }
+    return longest;
+}
 }
