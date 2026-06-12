79ae7da2547c4df69ea62b920fb67d48619d1f40
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-06-12 14:36:12 +0530

    Optimal approach of 3 Sum

--- Placement/Sum3.java
+++ Placement/Sum3.java
@@ -7,7 +7,8 @@ import java.util.HashSet;
 public class Sum3{
     public static void main(String[] args){
         int arr[]={-1,0,1,2,-1,-4};
-    System.out.println(S3(arr));
+    //System.out.println(S3(arr));
+    System.out.println(ThreeSum(arr));
     }
     public static ArrayList<ArrayList<Integer>> S3(int arr[]){
         HashSet<ArrayList<Integer>> fin= new HashSet<>();
@@ -28,4 +29,31 @@ public class Sum3{
     }
     return new ArrayList<>(fin);
 }
-}
+    public static ArrayList<ArrayList<Integer>> ThreeSum(int arr[]){
+        if(arr == null || arr.length<3 ){
+            return new ArrayList<>();
+        }
+        HashSet<ArrayList<Integer>> hs= new HashSet<>();
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-2;i++){
+            int left=i+1;
+            int right=arr.length-1;
+            while(left<right){
+                int sum=arr[i]+arr[left]+arr[right];
+
+                if(sum==0){
+                    hs.add(new ArrayList<>(Arrays.asList(arr[i],arr[left], arr[right])));
+                    left++;
+                    right--;
+                }else if(sum>0){
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+        return new ArrayList<>(hs);
+                }
+            }
+        
+    
