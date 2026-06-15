a18a1f2ca6d359587d7235a65f61514fd1371b40
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-06-15 14:35:51 +0530

    Understood find pivot elemet, using prefix sum

---
+++ Placement/FindPivot.java
@@ -0,0 +1,22 @@
+package Placement;
+public class FindPivot{
+    public static void main(String[] args){
+        int arr[]={1,7,3,6,5,6};
+        System.out.println(findP(arr));
+    }
+    public static int findP(int arr[]){
+        int sum=0;
+        for(int i:arr){
+            sum+=i;
+        }
+        int leftS=0;
+        for(int i=0;i<arr.length;i++){
+            int rightSum=sum-leftS-arr[i];
+            if(leftS==rightSum){
+                return i;
+            }
+            leftS+=arr[i];
+        }
+        return -1;
+    }
+}
